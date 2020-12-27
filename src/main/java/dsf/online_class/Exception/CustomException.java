6dac26f66b6272a0aead0a7b795e4886e9805fc7
DESKTOP-BFGEQUG\dsf <dsfgis@foxmail.com>
2020-12-27 21:29:03 +0800

    no message

--- src/main/java/dsf/online_class/Exception/CustomException.java
+++ src/main/java/dsf/online_class/Exception/CustomException.java
@@ -1,4 +1,23 @@
 package dsf.online_class.Exception;
 
-public class CustomException {
+public class CustomException extends RuntimeException{
+    private Integer code;
+
+    private String msg;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
 }
--- src/main/java/dsf/online_class/Exception/CustomExceptionHandler.java
+++ src/main/java/dsf/online_class/Exception/CustomExceptionHandler.java
@@ -1,4 +1,25 @@
 package dsf.online_class.Exception;
 
+import dsf.online_class.utils.JsonData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
 public class CustomExceptionHandler {
+
+    private final static Logger logger = LoggerFactory.getLogger(CustomException.class);
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public JsonData handler(Exception e){
+        logger.error("系统异常",e);
+        if(e instanceof CustomException){
+            CustomException customException =( CustomException) e;
+            return JsonData.buildError(customException.getCode(),customException.getMsg());
+        }else  {
+            return  JsonData.buildError("未知错误");
+        }
+    }
 }
