17dec1c035bada2805c023b11d0452c78914dc1a
DESKTOP-BFGEQUG\dsf <dsfgis@foxmail.com>
2020-12-27 22:03:35 +0800

    啊啊啊

--- src/main/java/dsf/online_class/controller/UserControlller.java
+++ src/main/java/dsf/online_class/controller/UserControlller.java
@@ -1,4 +1,21 @@
 package dsf.online_class.controller;
 
+
+import dsf.online_class.domain.User;
+import dsf.online_class.utils.JsonData;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("api/v1/pub/user")
 public class UserControlller {
+
+    @PostMapping("register")
+    public JsonData register(@RequestBody Map<String,String> userInfo){
+        
+    }
 }
