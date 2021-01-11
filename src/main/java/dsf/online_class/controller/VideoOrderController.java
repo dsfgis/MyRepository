1013b3ac9c62c99d88ec6b7e6a7f0fa65956be74
daishaofeng <dsfgis@hotmail.com>
2021-01-11 16:03:57 +0800

    no message

--- src/main/java/dsf/online_class/controller/VideoOrderController.java
+++ src/main/java/dsf/online_class/controller/VideoOrderController.java
@@ -2,8 +2,9 @@ package dsf.online_class.controller;
 
 
 import dsf.online_class.model.request.VideoOrderRequest;
+import dsf.online_class.service.VideoOrderService;
 import dsf.online_class.utils.JsonData;
-import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +16,14 @@ import javax.servlet.http.HttpServletRequest;
 
 public class VideoOrderController {
 
+    @Autowired
+    private  VideoOrderService videoOrderService;
     @RequestMapping("save")
     public JsonData saveOrder(@RequestBody VideoOrderRequest videoOrderRequest, HttpServletRequest request){
 
         Integer userId  = (Integer)request.getAttribute("user_id");
-        return JsonData.buildSuccess();
+        int rows = videoOrderService.save(userId,videoOrderRequest.getVideoId());
+        return 0==rows? JsonData.buildError("下单失败"):JsonData.buildSuccess("下单成功");
     }
 
 }
--- src/main/java/dsf/online_class/service/impl/VideoOrderServiceImpl.java
+++ src/main/java/dsf/online_class/service/impl/VideoOrderServiceImpl.java
@@ -1,14 +1,20 @@
 package dsf.online_class.service.impl;
 
+import dsf.online_class.mapper.VideoMapper;
 import dsf.online_class.mapper.VideoOrderMapper;
+import dsf.online_class.model.entity.Video;
 import dsf.online_class.model.entity.VideoOrder;
 import dsf.online_class.service.VideoOrderService;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.UUID;
 
 @Service
 public class VideoOrderServiceImpl implements VideoOrderService {
 
 
+    private VideoMapper videoMapper;
     private VideoOrderMapper videoOrderMapper;
     /**
      * 下单
@@ -23,6 +29,16 @@ public class VideoOrderServiceImpl implements VideoOrderService {
 
 
         if(null!= videoOrder){return 0;}
-        return 0;
+        Video video = videoMapper.findById(videoId);
+        VideoOrder newVideoOrder = new VideoOrder();
+        newVideoOrder.setCreateTime(new Date());
+        newVideoOrder.setOutTradeNo(UUID.randomUUID().toString());
+        newVideoOrder.setUserId(userId);
+        newVideoOrder.setVideoId(videoId);
+        newVideoOrder.setVideoImg(video.getCoverImg());
+        newVideoOrder.setVideoTitle(video.getTitle());
+        newVideoOrder.setTotalFree(video.getPrice());
+        int rows  = videoOrderMapper.saveOrder(newVideoOrder);
+        return rows;
     }
 }
